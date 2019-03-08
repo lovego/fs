da62265c15137fe5314e6fe3b2bd5fb8fb1ad08e
bughou <applejava@qq.com>
2019-03-08 17:06:00 +0800

    save

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# fs
+file system relevant utils.
+
+[![Build Status](https://travis-ci.org/lovego/fs.svg?branch=master)](https://travis-ci.org/lovego/fs)
+[![Coverage Status](https://img.shields.io/coveralls/github/lovego/fs/master.svg)](https://coveralls.io/github/lovego/fs?branch=master)
+[![Go Report Card](https://goreportcard.com/badge/github.com/lovego/fs)](https://goreportcard.com/report/github.com/lovego/fs)
+[![GoDoc](https://godoc.org/github.com/lovego/fs?status.svg)](https://godoc.org/github.com/lovego/fs)
+
+## Install
+`$ go get github.com/lovego/fs`
+
+
+## Documentation
+[https://godoc.org/github.com/lovego/fs](https://godoc.org/github.com/lovego/fs)
--- logfile_windows.go
+++ logfile_windows.go
@@ -5,10 +5,8 @@ package fs
 import (
 	"log"
 	"os"
-	"os/signal"
 	"path/filepath"
 	"sync"
-	"syscall"
 )
 
 type LogFile struct {
@@ -22,17 +20,6 @@ func NewLogFile(path string) (*LogFile, error) {
 	if err := l.open(); err != nil {
 		return nil, err
 	}
-	ch := make(chan os.Signal)
-	signal.Notify(ch, syscall.SIGUSR1)
-
-	go func() {
-		for {
-			<-ch
-			if err := l.open(); err != nil {
-				log.Println(err)
-			}
-		}
-	}()
 	return &l, nil
 }
 
@@ -44,8 +31,6 @@ func (l *LogFile) open() error {
 	if err != nil {
 		return err
 	}
-	l.Lock()
-	defer l.Unlock()
 	if l.File != nil {
 		if err := l.File.Close(); err != nil {
 			log.Println(err)
@@ -53,10 +38,4 @@ func (l *LogFile) open() error {
 	}
 	l.File = file
 	return nil
-}
-
-func (l *LogFile) Write(b []byte) (n int, err error) {
-	l.RLock()
-	defer l.RUnlock()
-	return l.File.Write(b)
 }
